6bcf9fe49e3a807889770eb0d947648f257430c9
Sandhya Areti <sandhya.areti@accenture.com>
2021-06-13 16:03:07 +0530

    Category change.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -6,7 +6,7 @@ namespace PartsUnlimited.Models
     public class Category
     {
         public int CategoryId { get; set; }
-
+        // my third commit
         [Required]
         public string Name { get; set; }
 
